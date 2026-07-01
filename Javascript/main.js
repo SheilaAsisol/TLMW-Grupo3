a77b0e76f9734512df78c30436cd0a984d16aab9
SheilaAsisol <nairasisol@gmail.com>
2026-07-01 09:17:48 -0300

    feat:modificacion del nav-index y agregue un formulario con validacion

--- Javascript/main.js
+++ Javascript/main.js
@@ -13,3 +13,21 @@ if (document.body.classList.contains("oscuro")) {
 }
   });
 }
+/*Validación de formulario*/
+const formulario = document.getElementById('contactoForm');
+const mensajeEstado = document.getElementById('mensajeEstado');
+
+formulario.addEventListener('submit', function(evento) {
+  evento.preventDefault();
+
+  const email = document.getElementById('email').value.trim();
+  const mensaje = document.getElementById('mensajeTexto').value.trim();
+
+  if ( email === '' || mensaje === '') {
+    mensajeEstado.innerHTML = '<p style="color: red;">¡Error! Completa todos los campos vacíos.</p>';
+    return; 
+  }
+
+  mensajeEstado.innerHTML = '<p style="color: green;">¡Mensaje enviado con éxito!</p>';
+  formulario.reset(); 
+});
